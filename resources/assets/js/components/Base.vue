55be1ef1de40b5b545da6cdd5f219b4c9870d364
brent_abenity <brent@abenity.com>
2017-12-03 22:08:59 -0600

    prevent multiple hits to pagination request

--- resources/assets/js/components/Base.vue
+++ resources/assets/js/components/Base.vue
@@ -24,7 +24,7 @@
             this.$store.dispatch('fetchArticles');
 
             window.onscroll = (ev) => {
-                if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight && !isLoading) {
+                if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight && !this.isLoading) {
                     this.isLoading = true;
                     this.$store.dispatch('fetchArticles').then((payload) => {
                         this.isLoading = false;
